8ff3976bdd60bdf31d423035d813459d5ec18e6e
Ange Chierchia <ange.chierchia@gmail.com>
2019-09-21 16:13:24 +0200

    update nav open animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,17 +10,34 @@ var replaceFormspreeEmail = function() {
 	emailLink.setAttribute("action", "https://formspree.io/" + emailName.split('').reverse().join('') + "@" + emailTLD.split('').reverse().join(''));
 };
 
+var navItemAnimDelay = function() {
+	var needAnimation = document.querySelectorAll('.nav-item[data-animate]');
+	needAnimation.forEach(function(elem, idx) {
+		elem.style.transitionDelay = 100 * (idx+1) + 'ms';
+	});
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 	if (document.querySelector(".contact-form")) {
 		replaceFormspreeEmail();
 	}
 
-	inView('[data-animate]').on('enter', function(selector) {
-		selector.classList.add('inview');
-	});
+	if (document.querySelector('[data-animate')) {
+		navItemAnimDelay();
+		inView('[data-animate]').on('enter', function(selector) {
+			selector.classList.add('inview');
+		});
+	}
 
 	navToggle.addEventListener('click', function() {
 		navToggle.classList.toggle('open');
 		body.classList.toggle('no-scroll');
+
+		if (!navToggle.classList.contains(open)) {
+			var navItems = document.querySelectorAll('.nav-item.inview');
+			navItems.forEach(function(selector) {
+				selector.classList.remove('inview');
+			});
+		}
 	});
 });
